495047cbfbce11851499cc9ceb149609e767192e
byeunghoon Kang <qudgns119@naver.com>
2025-01-05 23:14:22 +0900

    [Feat] 채팅방 회원 목록 조회시 필요한 메서드 추가

--- src/main/java/com/manchui/domain/repository/ChatRoomRepository.java
+++ src/main/java/com/manchui/domain/repository/ChatRoomRepository.java
@@ -4,4 +4,6 @@ import com.manchui.domain.entity.ChatRoom;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ChatRoomRepository extends JpaRepository<ChatRoom, Long> {
+
+    ChatRoom findByRoomId(String roomId);
 }
