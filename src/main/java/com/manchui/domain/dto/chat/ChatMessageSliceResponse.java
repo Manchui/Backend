8621f8f124ef3ab162cbb88d68c9641cb6e58148
byeunghoon Kang <qudgns119@naver.com>
2025-01-06 20:53:52 +0900

    [Feat] 채팅 메시지 조회(무한스크롤) 응답 DTO 추가

---
+++ src/main/java/com/manchui/domain/dto/chat/ChatMessageSliceResponse.java
@@ -0,0 +1,14 @@
+package com.manchui.domain.dto.chat;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.util.List;
+
+@Getter
+@AllArgsConstructor
+public class ChatMessageSliceResponse {
+
+    private List<ChatMessageResponse> chatMessageResponseList;
+    private Boolean hasNext;
+}
