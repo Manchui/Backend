62771df4ac64620446001e8484cb362cbe28b9c0
byeunghoon Kang <qudgns119@naver.com>
2025-01-06 20:52:06 +0900

    [Feat] 무한 스크롤을 위한 마지막 메시지Id를 기반으로 채팅 메시지를 가져오는 메서드 추가

--- src/main/java/com/manchui/domain/repository/mongodb/ChatMessageRepository.java
+++ src/main/java/com/manchui/domain/repository/mongodb/ChatMessageRepository.java
@@ -1,10 +1,15 @@
 package com.manchui.domain.repository.mongodb;
 
 import com.manchui.domain.entity.mongodb.ChatMessage;
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
 import reactor.core.publisher.Flux;
 
 public interface ChatMessageRepository extends ReactiveMongoRepository<ChatMessage, String> {
 
     Flux<ChatMessage> findByRoomIdOrderByCreatedAtDesc(String roomId);
+
+    @Query(value = "{ 'roomId': ?0, '_id': { $lt: ?1 } }", sort = "{ '_id': -1 }")
+    Flux<ChatMessage> findByRoomIdAndIdLessThanOrderByIdDesc(String roomId, ObjectId lastMessageId);
 }
