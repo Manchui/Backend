43c8cc6832be072508b2e5a55ee3fc23c94fa24a
byeunghoon Kang <qudgns119@naver.com>
2025-01-09 20:38:03 +0900

    [Feat] ChatRoomUser 엔티티 Timestaped 상속하도록 추가

--- src/main/java/com/manchui/domain/entity/ChatRoomUser.java
+++ src/main/java/com/manchui/domain/entity/ChatRoomUser.java
@@ -1,5 +1,6 @@
 package com.manchui.domain.entity;
 
+import com.manchui.global.entity.Timestamped;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Getter;
@@ -8,7 +9,7 @@ import lombok.NoArgsConstructor;
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class ChatRoomUser {
+public class ChatRoomUser extends Timestamped {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
