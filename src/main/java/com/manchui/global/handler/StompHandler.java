da31b11fdaf6d5ed436e0392b8afc62f7ea3a194
byeunghoon Kang <qudgns119@naver.com>
2025-02-05 23:18:41 +0900

    [Feat] STOMP에서 Authentication을 사용하기 위한 기능 추가

--- src/main/java/com/manchui/global/handler/StompHandler.java
+++ src/main/java/com/manchui/global/handler/StompHandler.java
@@ -1,5 +1,8 @@
 package com.manchui.global.handler;
 
+import com.manchui.domain.dto.CustomUserDetails;
+import com.manchui.domain.entity.User;
+import com.manchui.domain.repository.UserRepository;
 import com.manchui.domain.service.RedisRefreshTokenService;
 import com.manchui.global.exception.CustomException;
 import com.manchui.global.exception.ErrorCode;
@@ -15,6 +18,10 @@ import org.springframework.messaging.MessageChannel;
 import org.springframework.messaging.simp.stomp.StompCommand;
 import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.messaging.support.ChannelInterceptor;
+import org.springframework.messaging.support.MessageHeaderAccessor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -25,18 +32,25 @@ public class StompHandler implements ChannelInterceptor {
 
     private final JWTUtil jwtUtil;
     private final RedisRefreshTokenService redisRefreshTokenService;
+    private final UserRepository userRepository;
 
 
     // STOMP 메시지 전송 전에 가로채서 처리할 로직
     @Override
     public Message<?> preSend(Message<?> message, MessageChannel channel) {
         // STOMP 헤더를 핸들링하기 위한 액세서 생성
-        StompHeaderAccessor accessor = StompHeaderAccessor.wrap(message);
+        StompHeaderAccessor accessor = MessageHeaderAccessor.getAccessor(message, StompHeaderAccessor.class);
 
         // 만약 CONNECT 커맨드라면, JWT 검증 수행
         if(StompCommand.CONNECT.equals(accessor.getCommand())){
             String authorizationHeader = String.valueOf(accessor.getFirstNativeHeader("Authorization"));
             validateAccessToken(authorizationHeader);
+
+            // 인증 정보(Authentication)를 SecurityContext에서 가져와 STOMP 메시지의 사용자(User)로 설정
+            Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+            if (auth != null) {
+                accessor.setUser(auth); // STOMP 메시지의 Principal에 사용자 정보를 저장
+            }
         }
 
         // 정상 처리된 경우 메시지를 그대로 리턴
@@ -78,5 +92,14 @@ public class StompHandler implements ChannelInterceptor {
         if (!redisRefreshTokenService.existsByAccessToken(userEmail)) {
             throw new CustomException(ErrorCode.INVALID_ACCESS_TOKEN);
         }
+
+        User user = userRepository.findByEmail(userEmail);
+        CustomUserDetails customUserDetails = new CustomUserDetails(user);
+
+        // Authentication 객체 생성
+        Authentication authentication = new UsernamePasswordAuthenticationToken(customUserDetails, null, customUserDetails.getAuthorities());
+
+        // SecurityContext 에 등록
+        SecurityContextHolder.getContext().setAuthentication(authentication);
     }
 }
